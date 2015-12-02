7a3bd02d8c7671c60a70501a2510c0fddb12bbd5
yksz <user4git@gmail.com>
2015-12-02 10:54:23 +0900

    Modify actor_example.cpp to use struct as message

--- ccl/example/actor_example.cpp
+++ ccl/example/actor_example.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <typeinfo>
 #include <boost/any.hpp>
 #include "ccl/actor.h"
 
+namespace {
+
+struct Point {
+    int x;
+    int y;
+};
+
+} // unnamed namespace
+
 int main(void) {
     auto actor = std::make_shared<ccl::Actor>([](const boost::any& message) {
-        if (message.type() == typeid(int)) {
+        const std::type_info& type = message.type();
+        if (type == typeid(int)) {
             std::cout << "int: " << boost::any_cast<int>(message) << std::endl;
-        } else if (message.type() == typeid(double)) {
+        } else if (type == typeid(double)) {
             std::cout << "double: " << boost::any_cast<double>(message) << std::endl;
-        } else if (message.type() == typeid(char)) {
+        } else if (type == typeid(char)) {
             std::cout << "char: " << boost::any_cast<char>(message) << std::endl;
-        } else if (message.type() == typeid(char*)) {
+        } else if (type == typeid(char*)) {
             std::cout << "char*: " << boost::any_cast<char*>(message) << std::endl;
-        } else if (message.type() == typeid(std::string)) {
+        } else if (type == typeid(std::string)) {
             std::cout << "string: " << boost::any_cast<std::string>(message) << std::endl;
+        } else if (type == typeid(Point)) {
+            Point p = boost::any_cast<Point>(message);
+            std::cout << "point: x=" << p.x << ", y=" << p.y << std::endl;
         } else {
             std::cout << "others" << std::endl;
         }
@@ -27,5 +41,6 @@ int main(void) {
     system.Send("/path/actor1", (char*) "4");
     system.Send("/path/actor2", (char*) "5");
     system.Broadcast(std::string("6"));
+    system.Broadcast((Point) {7, 8});
     return 0;
 }
