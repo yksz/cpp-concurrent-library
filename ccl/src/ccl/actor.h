bd23e580891803f072d1f98cdabe84564f9ddcae
yksz <user4git@gmail.com>
2016-01-06 12:50:38 +0900

    Enable Actor to use thread pool

--- ccl/src/ccl/actor.h
+++ ccl/src/ccl/actor.h
@@ -1,92 +1,43 @@
 #pragma once
 
-#include <atomic>
-#include <condition_variable>
 #include <functional>
 #include <future>
 #include <map>
 #include <memory>
 #include <mutex>
-#include <queue>
 #include <string>
 #include <thread>
 #include "ccl/any.h"
+#include "ccl/thread_pool.h"
 
 namespace ccl {
 
 class Actor final {
 public:
-    explicit Actor(std::function<any(const any&)>&& onReceive);
-    ~Actor();
+    explicit Actor(std::function<any(const any&)>&& onReceive)
+            : m_onReceive(std::move(onReceive)), m_pool(std::make_shared<ThreadPool>(1)) {}
+    Actor(const std::shared_ptr<ThreadPool>& pool, std::function<any(const any&)>&& onReceive)
+            : m_onReceive(std::move(onReceive)), m_pool(pool) {}
+    ~Actor() = default;
     Actor(const Actor&) = delete;
     Actor& operator=(const Actor&) = delete;
 
     std::future<any> Send(const any& message);
     void SetShutdownNow(bool shutdownNow) {
-        m_shutdownNow = shutdownNow;
+        m_pool->SetShutdownNow(shutdownNow);
     }
 
 private:
     const std::function<any(const any&)> m_onReceive;
-    std::atomic<bool> m_shutdownNow;
-    bool m_stopped;
-    std::thread* m_thread;
-    std::queue<std::packaged_task<any()>> m_mailbox;
-    std::condition_variable m_condition;
-    std::mutex m_mutex;
+    std::shared_ptr<ThreadPool> m_pool;
 };
 
-inline Actor::Actor(std::function<any(const any&)>&& onReceive)
-        : m_onReceive(std::move(onReceive)), m_shutdownNow(false), m_stopped(false) {
-    auto worker = [this]() {
-        while (true) {
-            std::packaged_task<any()> task;
-            {
-                std::unique_lock<std::mutex> lock(m_mutex);
-                while (!m_stopped && m_mailbox.empty()) {
-                    m_condition.wait(lock);
-                }
-                if (m_stopped && m_mailbox.empty()) {
-                    return;
-                }
-                task = std::move(m_mailbox.front());
-                m_mailbox.pop();
-            }
-            task();
-        }
-    };
-    m_thread = new std::thread(std::move(worker));
-}
-
-inline Actor::~Actor() {
-    {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_stopped = true;
-        if (m_shutdownNow) {
-            // clear
-            while (!m_mailbox.empty()) {
-                m_mailbox.pop();
-            }
-        }
-    }
-    m_condition.notify_one();
-    m_thread->join();
-    delete m_thread;
-    m_thread = nullptr;
-}
-
 inline std::future<any> Actor::Send(const any& message) {
-    std::future<any> future;
-    {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        std::packaged_task<any()> task([this, message]() {
-            return m_onReceive(message);
-        });
-        future = task.get_future();
-        m_mailbox.push(std::move(task));
-    }
-    m_condition.notify_one();
-    return std::move(future);
+    auto task = std::make_shared<std::packaged_task<any()>>([this, message]() {
+        return m_onReceive(message);
+    });
+    m_pool->Dispatch([task]() { (*task)(); });
+    return task->get_future();
 }
 
 
