44cf1768bc7a94f52df94e5736554cda31c8fa04
yksz <user4git@gmail.com>
2016-01-13 23:52:03 +0900

    Add the Await method with timeout to countdown_latch.h

--- ccl/src/ccl/countdown_latch.h
+++ ccl/src/ccl/countdown_latch.h
@@ -12,6 +12,8 @@ public:
     CountdownLatch(const CountdownLatch&) = delete;
     CountdownLatch& operator=(const CountdownLatch&) = delete;
 
+    template<class Rep, class Period>
+    void Await(const std::chrono::duration<Rep, Period>& timeout);
     void Await();
     void CountDown();
     unsigned int GetCount();
@@ -21,6 +23,16 @@ private:
     std::condition_variable m_condition;
     std::mutex m_mutex;
 };
+
+template<class Rep, class Period>
+inline void CountdownLatch::Await(const std::chrono::duration<Rep, Period>& timeout) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    while (m_count > 0) {
+        if (m_condition.wait_for(lock, timeout) == std::cv_status::timeout) {
+            return;
+        }
+    }
+}
 
 inline void CountdownLatch::Await() {
     std::unique_lock<std::mutex> lock(m_mutex);
--- ccl/test/countdown_latch_test.cpp
+++ ccl/test/countdown_latch_test.cpp
@@ -13,17 +13,29 @@ TEST(CountdownLatch, AwaitAndCountDown) {
 
     // when:
     CountdownLatch latch(count);
-    std::vector<std::thread> threads;
     for (int i = 0; i < count; i++) {
         std::thread th([&]() {
             util::doHeavyTask();
             latch.CountDown();
         });
         th.detach();
-        threads.emplace_back(std::move(th));
     }
     latch.Await();
 
     // then:
     EXPECT_EQ(0, latch.GetCount());
+}
+
+TEST(CountdownLatch, AwaitAndTimeOut) {
+    // when:
+    CountdownLatch latch;
+    std::thread th([&]() {
+        latch.Await(std::chrono::milliseconds(100));
+        latch.CountDown();
+    });
+    th.detach();
+    latch.Await(std::chrono::milliseconds(10));
+
+    // then:
+    EXPECT_EQ(1, latch.GetCount());
 }
