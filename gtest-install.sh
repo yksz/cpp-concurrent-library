9f859ab722d9d044a4c74864913da64665a04242
yksz <user4git@gmail.com>
2016-02-17 14:43:55 +0900

    Refactor gtest-install.sh

--- gtest-install.sh
+++ gtest-install.sh
@@ -4,22 +4,23 @@ VERSION='1.7.0'
 INSTALL_DIR='/usr/local'
 DOWNLOAD_DIR='/tmp'
 
-wget -P ${DOWNLOAD_DIR} https://github.com/google/googletest/archive/release-${VERSION}.tar.gz
-cd ${DOWNLOAD_DIR}
+# Download
+cd $DOWNLOAD_DIR
+wget https://github.com/google/googletest/archive/release-${VERSION}.tar.gz
 tar xzvf release-${VERSION}.tar.gz
+
+# Build
 cd googletest-release-${VERSION}
-mkdir build
-cd build
-cmake ..
+cmake .
 make
 
 # Install
 sudo mkdir -p ${INSTALL_DIR}/include
-sudo cp -r ../include/gtest ${INSTALL_DIR}/include
+sudo cp -r include/gtest ${INSTALL_DIR}/include
 sudo mkdir -p ${INSTALL_DIR}/lib
 sudo cp *.a ${INSTALL_DIR}/lib
 
 # Cleanup
-cd ${DOWNLOAD_DIR}
+cd $DOWNLOAD_DIR
 rm release-${VERSION}.tar.gz
 rm -r googletest-release-${VERSION}
