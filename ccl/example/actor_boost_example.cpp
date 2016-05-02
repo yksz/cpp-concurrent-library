30f3b99e2b64cbae5fbdef8458740d07cc57824a
yksz <yksz.dev@gmail.com>
2016-05-02 13:12:37 +0900

    example/actor: Update actor examples

--- ccl/example/actor_boost_example.cpp
+++ ccl/example/actor_boost_example.cpp
@@ -1,8 +1,8 @@
 #include <future>
 #include <iostream>
 #include <typeinfo>
-#define CCL_ACTOR_DEBUG
-#include "ccl/actor_boost.h"
+#define CCL_ANY_USING_BOOST
+#include "ccl/actor.h"
 
 namespace {
 
@@ -14,50 +14,39 @@ struct Point {
 } // unnamed namespace
 
 int main(void) {
-    auto actor = std::make_shared<ccl::Actor>([](boost::any&& msg) -> boost::any {
+    using namespace boost;
+
+    auto actor = std::make_shared<ccl::Actor>([](any&& msg) -> any {
         const std::type_info& type = msg.type();
         if (type == typeid(int)) {
-            std::cout << "int: " << boost::any_cast<int>(msg) << std::endl;
+            std::cout << "int: " << any_cast<int>(msg) << std::endl;
         } else if (type == typeid(double)) {
-            std::cout << "double: " << boost::any_cast<double>(msg) << std::endl;
+            std::cout << "double: " << any_cast<double>(msg) << std::endl;
         } else if (type == typeid(char)) {
-            std::cout << "char: " << boost::any_cast<char>(msg) << std::endl;
+            std::cout << "char: " << any_cast<char>(msg) << std::endl;
         } else if (type == typeid(char*)) {
-            std::cout << "char*: " << boost::any_cast<char*>(msg) << std::endl;
-            return (char*) "char*: ok";
+            std::cout << "char*: " << any_cast<char*>(msg) << std::endl;
         } else if (type == typeid(std::string)) {
-            std::cout << "string: " << boost::any_cast<std::string>(msg) << std::endl;
+            std::cout << "string: " << any_cast<std::string>(msg) << std::endl;
         } else if (type == typeid(Point)) {
-            Point& p = boost::any_cast<Point&>(msg);
+            Point& p = any_cast<Point&>(msg);
             std::cout << "Point: x=" << p.x << ", y=" << p.y << std::endl;
         } else {
             std::cout << "others" << std::endl;
+            return std::string("others: ok");
         }
         return 0;
     });
     actor->Send(1);
     actor->Send(2.0);
     actor->Send('3');
-
-    std::future<boost::any> future1;
-    std::future<boost::any> future2;
-    {
-        ccl::ActorSystem system;
-        system.Register(actor, "/path/actor1");
-        future1 = system.Send((char*) "4", "/path/actor1");
-        future2 = system.Send((char*) "5", "/path/actor2");
-        system.Broadcast(std::string("6"));
-        system.Broadcast(Point{7, 8});
-        system.Broadcast(9L);
-    }
-    if (future1.valid()) {
-        boost::any response1 = future1.get();
-        if (response1.type() == typeid(char*)) {
-            std::cout << "response1: " << boost::any_cast<char*>(response1) << std::endl;
-        }
-    }
-    if (future2.valid()) {
-        std::cout << "response2" << std::endl;
+    actor->Send((char*) "4");
+    actor->Send(std::string("5"));
+    actor->Send(Point{6, 7});
+    auto future = actor->Send(8L);
+    auto response = future.get();
+    if (response.type() == typeid(std::string)) {
+        std::cout << "response: " << any_cast<std::string>(response) << std::endl;
     }
     return 0;
 }
--- ccl/example/actor_example.cpp
+++ ccl/example/actor_example.cpp
@@ -1,7 +1,6 @@
 #include <future>
 #include <iostream>
 #include <typeinfo>
-#define CCL_ACTOR_DEBUG
 #include "ccl/actor.h"
 
 namespace {
@@ -14,50 +13,39 @@ struct Point {
 } // unnamed namespace
 
 int main(void) {
-    auto actor = std::make_shared<ccl::Actor>([](ccl::any&& msg) -> ccl::any {
+    using namespace ccl;
+
+    auto actor = std::make_shared<ccl::Actor>([](any&& msg) -> any {
         const std::type_info& type = msg.type();
         if (type == typeid(int)) {
-            std::cout << "int: " << ccl::any_cast<int>(msg) << std::endl;
+            std::cout << "int: " << any_cast<int>(msg) << std::endl;
         } else if (type == typeid(double)) {
-            std::cout << "double: " << ccl::any_cast<double>(msg) << std::endl;
+            std::cout << "double: " << any_cast<double>(msg) << std::endl;
         } else if (type == typeid(char)) {
-            std::cout << "char: " << ccl::any_cast<char>(msg) << std::endl;
+            std::cout << "char: " << any_cast<char>(msg) << std::endl;
         } else if (type == typeid(char*)) {
-            std::cout << "char*: " << ccl::any_cast<char*>(msg) << std::endl;
-            return (char*) "char*: ok";
+            std::cout << "char*: " << any_cast<char*>(msg) << std::endl;
         } else if (type == typeid(std::string)) {
-            std::cout << "string: " << ccl::any_cast<std::string>(msg) << std::endl;
+            std::cout << "string: " << any_cast<std::string>(msg) << std::endl;
         } else if (type == typeid(Point)) {
-            Point& p = ccl::any_cast<Point&>(msg);
+            Point& p = any_cast<Point&>(msg);
             std::cout << "Point: x=" << p.x << ", y=" << p.y << std::endl;
         } else {
             std::cout << "others" << std::endl;
+            return std::string("others: ok");
         }
         return 0;
     });
     actor->Send(1);
     actor->Send(2.0);
     actor->Send('3');
-
-    std::future<ccl::any> future1;
-    std::future<ccl::any> future2;
-    {
-        ccl::ActorSystem system;
-        system.Register(actor, "/path/actor1");
-        future1 = system.Send((char*) "4", "/path/actor1");
-        future2 = system.Send((char*) "5", "/path/actor2");
-        system.Broadcast(std::string("6"));
-        system.Broadcast(Point{7, 8});
-        system.Broadcast(9L);
-    }
-    if (future1.valid()) {
-        ccl::any response1 = future1.get();
-        if (response1.type() == typeid(char*)) {
-            std::cout << "response1: " << ccl::any_cast<char*>(response1) << std::endl;
-        }
-    }
-    if (future2.valid()) {
-        std::cout << "response2" << std::endl;
+    actor->Send((char*) "4");
+    actor->Send(std::string("5"));
+    actor->Send(Point{6, 7});
+    auto future = actor->Send(8L);
+    auto response = future.get();
+    if (response.type() == typeid(std::string)) {
+        std::cout << "response: " << any_cast<std::string>(response) << std::endl;
     }
     return 0;
 }
