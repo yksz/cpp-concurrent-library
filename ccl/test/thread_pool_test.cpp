15385ac6f9f020c56605b29b4e0204110bcd1759
yksz <yksz.dev@gmail.com>
2016-05-11 02:05:25 +0900

    test/thread_pool: Add the test case of runtime nthreads

--- ccl/test/thread_pool_test.cpp
+++ ccl/test/thread_pool_test.cpp
@@ -1,6 +1,8 @@
 #include "ccl/thread_pool.h"
 #include <atomic>
 #include <chrono>
+#include <mutex>
+#include <set>
 #include <thread>
 #include <gtest/gtest.h>
 #include "util.h"
@@ -9,31 +11,40 @@ using namespace ccl;
 
 TEST(ThreadPool, Dispatch) {
     // setup:
+    const int nthreads = 10;
     const int dispatchCount = 1000;
-    std::atomic<int> count(0);
+    std::mutex mutex;
+    std::set<std::thread::id> threadIDs;
+    int count = 0;
 
     // when:
     {
-        ThreadPool pool(10);
+        ThreadPool pool(nthreads);
         for (int i = 0; i < dispatchCount; i++) {
             pool.Dispatch([&]() {
-                count++;
+                {
+                    std::lock_guard<std::mutex> lock(mutex);
+                    threadIDs.insert(std::this_thread::get_id());
+                    count++;
+                }
             });
         }
     }
 
     // then:
+    EXPECT_GE(nthreads, threadIDs.size());
     EXPECT_EQ(dispatchCount, count);
 }
 
 TEST(ThreadPool, ShutdownNow) {
     // setup:
+    const int nthreads = 10;
     const int dispatchCount = 1000;
     std::atomic<int> count(0);
 
     // when:
     {
-        ThreadPool pool(10);
+        ThreadPool pool(nthreads);
         pool.SetShutdownNow(true);
         for (int i = 0; i < dispatchCount; i++) {
             pool.Dispatch([&]() {
